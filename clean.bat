0d9f668fe27d43ef1b84b78731e8f23f56a63bd3
godspeed1989 <godspeed1989@gmail.com>
2014-11-28 20:52:47 +0800

    misc updates

--- clean.bat
+++ clean.bat
@@ -1,8 +1,9 @@
-del /Q main.aux
-del /Q main.lof
-del /Q main.log
-del /Q main.lot
-del /Q main.out
-del /Q main.toc
-del /Q main.synctex.gz
+@del /Q main.aux
+@del /Q main.lof
+@del /Q main.log
+@del /Q main.lot
+@del /Q main.out
+@del /Q main.toc
+@del /Q main.synctex.gz
+@del /Q main.pdf
 
